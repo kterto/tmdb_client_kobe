8526cccb2742b28effbca2ca1a75b1fba5130d18
Kaina Terto <kterto@capyba.com>
2019-11-25 07:51:45 -0300

    sentry

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,63 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:tmdb_client_kobe/src/locator.dart';
+import 'package:sentry/sentry.dart';
 
+import 'package:tmdb_client_kobe/src/locator.dart';
 import 'src/app.dart';
 
 void main() {
   Provider.debugCheckInvalidValueType = null;
+  runZoned<Future<void>>(() async {
+    setUpLocator();
+    runApp(App());
+  }, onError: (error, stackTrace) {
+    // Whenever an error occurs, call the `_reportError` function. This sends
+    // Dart errors to the dev console or Sentry depending on the environment.
+    _reportError(error, stackTrace);
+  });
 
-  setUpLocator();
-  runApp(App());
+  // This captures errors reported by the Flutter framework.
+  FlutterError.onError = (FlutterErrorDetails details) {
+    if (isInDebugMode) {
+      // In development mode, simply print to console.
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      // In production mode, report to the application zone to report to
+      // Sentry.
+      Zone.current.handleUncaughtError(details.exception, details.stack);
+    }
+  };
+}
+
+final SentryClient _sentry = SentryClient(
+    dsn: "https://b8fe6bb4c63a44618c0e4cd133530aa7@sentry.io/1832968");
+
+bool get isInDebugMode {
+  // Assume you're in production mode.
+  bool inDebugMode = false;
+
+  // Assert expressions are only evaluated during development. They are ignored
+  // in production. Therefore, this code only sets `inDebugMode` to true
+  // in a development environment.
+  assert(inDebugMode = true);
+
+  return inDebugMode;
+}
+
+Future<void> _reportError(dynamic error, dynamic stackTrace) async {
+  // Print the exception to the console.
+  print('Caught error: $error');
+  if (isInDebugMode) {
+    // Print the full stacktrace in debug mode.
+    print(stackTrace);
+    return;
+  } else {
+    // Send the Exception and Stacktrace to Sentry in Production mode.
+    _sentry.captureException(
+      exception: error,
+      stackTrace: stackTrace,
+    );
+  }
 }
